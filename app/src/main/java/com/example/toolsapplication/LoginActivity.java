22f3c103be2359b028ce519f0472480b0b5d888b
deep1360 <adeeps1360@gmail.com>
2020-07-05 13:09:04 -0400

    Amandeep Singh: Login With Firebase

--- app/src/main/java/com/example/toolsapplication/LoginActivity.java
+++ app/src/main/java/com/example/toolsapplication/LoginActivity.java
@@ -1,14 +1,62 @@
 package com.example.toolsapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class LoginActivity extends AppCompatActivity {
 
+    EditText email,password;
+    Button Login;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        email = findViewById(R.id.email);
+        password = findViewById(R.id.password);
+        Login = findViewById(R.id.Login);
+
+        Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                    LoginUser();
+            }
+        });
+    }
+
+    private void LoginUser() {
+        Login.setEnabled(false);
+        String user_email = email.getText().toString().trim();
+        String user_password = password.getText().toString().trim();
+
+        FirebaseAuth mAuth = FirebaseAuth.getInstance();
+
+        mAuth.signInWithEmailAndPassword(user_email, user_password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            Login.setEnabled(true);
+                            Toast.makeText(LoginActivity.this,"Login Successfull!", Toast.LENGTH_SHORT).show();
+                            Intent home = new Intent(LoginActivity.this,MainActivity.class);
+                            startActivity(home);
+                        } else {
+                            Login.setEnabled(true);
+                            Toast.makeText(LoginActivity.this, task.getException().getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 }
