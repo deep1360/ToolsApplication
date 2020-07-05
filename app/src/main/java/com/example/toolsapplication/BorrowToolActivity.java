0a9600d840020bc286f2fb7ae8afc7486f0a62f2
sandeepsssss <sandeepsinghsidhu099@gmail.com>
2020-07-04 21:42:05 -0400

    Sandeep: Created Activity Borrow Tool Design

---
+++ app/src/main/java/com/example/toolsapplication/BorrowToolActivity.java
@@ -0,0 +1,14 @@
+package com.example.toolsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class BorrowToolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_borrow_tool);
+    }
+}
