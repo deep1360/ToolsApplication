68b15faaf88e54340443dd62aef97654923582c7
Amandeepsuggal <amandeep_suggal@yahoo.com>
2020-07-04 21:54:41 -0400

    Aman Kaur: Design Posting Tool Activity

---
+++ app/src/main/java/com/example/toolsapplication/PostToolActivity.java
@@ -0,0 +1,14 @@
+package com.example.toolsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PostToolActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_post_tool);
+    }
+}
