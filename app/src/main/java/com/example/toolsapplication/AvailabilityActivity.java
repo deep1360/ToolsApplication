5cf6564020d678304470882c304328f95589aa33
harbir22 <hsdhillon0351@gmail.com>
2020-07-05 22:45:58 -0400

    Harbir: Availability Displayed on Screen

--- app/src/main/java/com/example/toolsapplication/AvailabilityActivity.java
+++ app/src/main/java/com/example/toolsapplication/AvailabilityActivity.java
@@ -1,14 +1,94 @@
 package com.example.toolsapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class AvailabilityActivity extends AppCompatActivity {
+
+    TextView Tool_Name,Tool_Owner,Owner_Email,Tool_Status;
+
+    Button borrowBtn, favButton;
+
+    String toolid = "", tname = "", tuser = "", temail = "", userid = "", desc = "";
+    boolean tstatus;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_availability);
+
+        Tool_Name = findViewById(R.id.toolName);
+        Tool_Owner = findViewById(R.id.toolOwner);
+        Owner_Email = findViewById(R.id.Emailtoolowner);
+        Tool_Status = findViewById(R.id.ToolStatus);
+        borrowBtn = findViewById(R.id.toolBorrowButton);
+        favButton = findViewById(R.id.AddButtonFavorite);
+
+
+        if(getIntent()!=null){
+            toolid = getIntent().getStringExtra("id");
+            tname = getIntent().getStringExtra("toolname");
+            tuser = getIntent().getStringExtra("username");
+            temail = getIntent().getStringExtra("useremail");
+            userid = getIntent().getStringExtra("userid");
+            tstatus = getIntent().getBooleanExtra("available",false);
+            desc = getIntent().getStringExtra("desc");
+
+            Tool_Name.setText(tname);
+            Owner_Email.setText(temail);
+            Tool_Owner.setText(tuser);
+
+            if(tstatus){
+                Tool_Status.setText("Available");
+            }
+            else {
+                Tool_Status.setText("Not Available");
+                borrowBtn.setEnabled(false);
+            }
+
+        }
+
+        borrowBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(AvailabilityActivity.this,BorrowToolActivity.class);
+                intent.putExtra("id",toolid);
+                intent.putExtra("toolname",tname);
+                intent.putExtra("username",tuser);
+                intent.putExtra("useremail",temail);
+                intent.putExtra("available",tstatus);
+                intent.putExtra("userid",userid);
+                intent.putExtra("desc",desc);
+                startActivity(intent);
+            }
+        });
+
+        favButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+            }
+        });
+
+
     }
 }
